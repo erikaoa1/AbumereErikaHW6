7b2880502018d84e0bf32f9651c84162e8f559f2
Erika Abumere <erikaabumere@gmail.com>
2023-10-28 21:44:22 -0700

    c

---
+++ AbumereErikaHW6/FavoritesFlashcardListPage.swift
@@ -0,0 +1,31 @@
+//
+//  FavoritesFlashcardListPage.swift
+//  AbumereErikaHW6
+//
+//  Created by Student on 10/28/23.
+//
+
+import Foundation
+import SwiftUI
+struct FavoritesFlashcardListPage: View {
+    @EnvironmentObject var flashcardViewModel: FlashcardViewModel
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(flashcardViewModel.flashcards.filter { $0.isFavorite }) { flashcard in
+                    NavigationLink(destination: EditFlashCardPage(flashcard: flashcard)) {
+                        FlashcardCell(flashcard: flashcard)
+                    }
+                }
+            }
+            .navigationTitle("Favorites")
+        }
+    }
+}
+
+struct FavoritesFlashcardListPage_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesFlashcardListPage()
+    }
+}
